958ac768ae0a433b7178001b2c3767385f150b51
scocomakoko <70449335+scocomakoko@users.noreply.github.com>
2023-07-08 00:03:50 +0300

    add move to the paddle

--- Assets/Scripts/FollowVerticly.cs
+++ Assets/Scripts/FollowVerticly.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class FollowVerticly : MonoBehaviour
 {
     public GameObject target;
+    public float speed;
+    public float maxSpeed = 45;
+    public float minSpeed = 30;
+    public float slipChance = 1;
+    private Rigidbody2D moveRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
+        moveRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+        speed += Random.Range(-0.12f, 0.1f);
+        speed = Mathf.Max(Mathf.Min(speed, maxSpeed), minSpeed);
+        float distance = target.transform.position.y - transform.position.y;
+        Vector2 force = Vector2.up * speed * distance/Mathf.Abs(distance);
+        float slip = Random.Range(0,100);
+        if (slip < slipChance)
+        {
+            force *= 0.1f;
+        }
+        moveRigidbody.AddForce(force);
     }
 }
